091352b9aef499dbbc0af9d8faa0239a352474e4
sboriskirov <borisasenovkirov@gmail.com>
2020-05-10 15:13:24 +0200

    meta

--- pages/index.js
+++ pages/index.js
@@ -34,12 +34,12 @@ function Home() {
           <meta name="og:title" property="og:title" content="Boris Kirov - Product Designer" />
           <meta name="og:description" property="og:description" content="Technical designer interested in systems, front-end, open source and collaborative design." />
           <meta property="og:site_name" content="Boris Kirov - Product designer" />
-          <meta property="og:url" content="https://www.boriskirov.com" />  
+          <meta property="og:url" content="https://www.boriskirov.cc" />  
           <meta property="og:image" content="/favicon.png" />  
           <meta name="twitter:card" content="summary" /> 
           <meta name="twitter:title" content="Boris Kirov - Product Designer" />
           <meta name="twitter:description" content="Technical designer interested in systems, front-end, open source and collaborative design." />
-          <meta name="twitter:site" content="https://www.boriskirov.com" />
+          <meta name="twitter:site" content="https://www.boriskirov.cc" />
           <meta name="twitter:creator" content="Boris Kirov" />
           <meta name="twitter:image" content="/favicon.png" />
           <link rel="apple-touch-icon" href="/favicon.png" />
