c61bbd91f042bee2cc9d1fee0350cb358a552619
Boris Kirov <borisasenovkirov@gmail.com>
2021-02-04 14:26:40 +0100

    typo

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ function Home() {
                   Design and prototype in the browser 
                 </li>
                 <li>
-                  Fluididty
+                  Fluidity
                 </li>
                 <li>
                   Maintenance & experiments with design and experience consistency 
