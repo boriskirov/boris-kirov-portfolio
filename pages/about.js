9a37c9e0694c3474171b9dd04b3a84db774ba85b
Boris <borisasenovkirov@gmail.com>
2021-10-14 17:27:26 +0200

    fixed typos on about me

--- pages/about.js
+++ pages/about.js
@@ -37,10 +37,11 @@ const About = () => (
           </section>
           <section>
             <p>
-              <span className="dropCap">M</span> y name is Boris Kirov. I'm a
-              Bulgarian designer living in Amsterdam, The Netherlands. I use
-              technology to express myself and I'm currently building the Miro
-              Design system and Platform experience.
+              <span className="dropCap">M</span>y name is Boris Kirov. I'm a
+              Bulgarian designer living in Amsterdam, The Netherlands. My care
+              for form and function makes me express myself with technology.
+              Currently, I'm building the Miro Platform experience and Design
+              system.
             </p>
             <p>
               Before joining Miro I used to work with different companies like{" "}
@@ -54,14 +55,14 @@ const About = () => (
               and clients like Disney, BNP Paribas, Graydon & Tripscout. In my
               spare time, I enjoy exploring technology and building tools to
               empower others to build and progress. I am also fascinated by
-              programs and how they can communicate between eachother and with
-              us. My passion for constant experimentation in the realm of design
-              and experience consistency keeps my busy and always hungry. You
-              can always reach me via{" "}
+              programs and how they can communicate with each other and with us.
+              My passion for constant experimentation in the realm of design and
+              experience consistency keeps me busy and always hungry. You can
+              always reach me via{" "}
               <a href="https://twitter.com/sboriskirov" target="_blank">
                 Twitter
               </a>{" "}
-              or through booking a session on my{" "}
+              or by booking a session on my{" "}
               <a
                 href="https://www.adplist.org/mentors/boris-kirov"
                 target="_blank"
@@ -71,23 +72,23 @@ const About = () => (
               profile.
             </p>
             <p>
-              Leaving time for sports like running, cycling and jump rope is
-              essential and important for my physical and mental health. But the
-              most important one is the time for long walks with our lovely
-              monochromatic Mozilla firefox looking Dory. She is an amazingly
-              smart and incredibly responsive adopted mixed-breed dog.
+              Leaving time for sports like running, cycling, and jump rope is
+              essential for my physical and mental health. But the most
+              important one is the time for long walks with our lovely
+              monochromatic, Mozilla firefox-looking, Dory. She is an amazing,
+              smart, and incredibly responsive adopted mixed-breed dog.
             </p>
             <img src="/dory.png" />
             <p>
               Photography also is a passion of mine, with which I keep my
               creative juices flowing. I am fascinated by film photography and
-              currently I'm also deep into drone photography. You can check some
-              of my flickaronies on my{" "}
+              currently, I'm also deep into drone photography. You can check
+              some of my flickaronies on{" "}
               <a href="https://instagram.com/yng.blds" target="_blank">
                 Instagram
               </a>
               . Before I used to collaborate with clients like Tea House Sofia,
-              FITC Amsterdam and others.{" "}
+              FITC Amsterdam, and others.
             </p>
           </section>
           <section>
