b68e982833f7826d26c16736cb3e268ad426ea50
Boris <borisasenovkirov@gmail.com>
2021-11-06 16:48:27 +0100

    fixed the feedback form

--- components/feedbackForm.js
+++ components/feedbackForm.js
@@ -3,6 +3,7 @@ import Card from "./card";
 const FeedbackForm = ({ description }) => {
   return (
     <div className="contentWrapper">
+      <hr />
       <Card
         link="https://twitter.com/sboriskirov"
         className="external mainCard"
