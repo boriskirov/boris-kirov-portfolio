bd1a2aaf2b12f7822715e23811dcc6b1568bc0e6
Boris <borisasenovkirov@gmail.com>
2022-02-19 16:45:16 +0100

    clipboarAPI for chromium fix

--- components/Colorschema/color-palette.js
+++ components/Colorschema/color-palette.js
@@ -6,32 +6,38 @@ import ColorData from "../../components/Colorschema/color-schema-data";
 // the idea here that the first function will create a scope which will hold the value
 // so by the time the inner function is called it will have access to value inside
 function copyHex(hex) {
-  return function () {
-    navigator.clipboard.writeText(`${hex}`);
-    alert(`Copied ${hex}`);
+  return async function () {
+    try {
+      await navigator.clipboard.writeText(`${hex}`);
+      alert(`Copied ${hex}`);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
   };
 }
 
-const Colors = () => (
-  <div className="contentWrapper">
-    {ColorData.map((colorschema) => {
-      return (
-        <button
-          className="color-wrapper dark-text"
-          type="button"
-          key={colorschema.code}
-          style={{
-            backgroundColor: colorschema.code,
-          }}
-          // onClick needs a function, so we call our function
-          // which will the return a function to be used by onClick
-          onClick={copyHex(colorschema.code)}
-        >
-          <span className={colorschema.tag}>{colorschema.code}</span>
-        </button>
-      );
-    })}
-  </div>
-);
+function Colors() {
+  return (
+    <div className="contentWrapper">
+      {ColorData.map((colorschema) => {
+        return (
+          <button
+            className="color-wrapper dark-text"
+            type="button"
+            key={colorschema.code}
+            style={{
+              backgroundColor: colorschema.code,
+            }}
+            // onClick needs a function, so we call our function
+            // which will the return a function to be used by onClick
+            onClick={copyHex(colorschema.code)}
+          >
+            <span className={colorschema.tag}>{colorschema.code}</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+}
 
 export default Colors;
