d9da6b798ddd5ae9c824a674cbd71940c3065e08
Boris Kirov <borisasenovkirov@gmail.com>
2022-04-07 14:07:02 +0200

    text switch test

--- components/header.js
+++ components/header.js
@@ -1,59 +1,17 @@
 import React from "react";
-
-function DarkMode() {
-  const [mode, setMode] = React.useState("light");
-
-  React.useEffect(() => {
-    const theme = window.localStorage.getItem("__theme__");
-    if (theme) {
-      setMode(() => theme);
-      if (theme === "dark") {
-        document.documentElement.classList.add("dark");
-      }
-    } else {
-      setMode((m) => (m === "light" ? "dark" : "light"));
-      window.localStorage.setItem("__theme__", mode);
-    }
-  }, []);
-
-  const onClick = () => {
-    setMode((m) => {
-      const newTheme = m === "light" ? "dark" : "light";
-      document.documentElement.classList.toggle("dark");
-      window.localStorage.setItem("__theme__", newTheme);
-
-      return newTheme;
-    });
-  };
-
-  return (
-    <button
-      type="button"
-      aria-label="Theme toggle"
-      className="theme-toggle"
-      onClick={onClick}
-    ></button>
-  );
-}
-
-export function AmsterdamTime() {
-  const TimeFomatter = new Intl.DateTimeFormat("en-GB", {
-    timeZone: "Europe/Amsterdam",
-    hour: "numeric",
-    minute: "numeric",
-    weekday: "short",
-    hour12: false,
-  }).format();
-  return <small>{TimeFomatter}, Amsterdam</small>;
-}
+import FontSwap from "../components/font-switch";
+import DarkMode from "../components/theme-switch";
+import AmsterdamTime from "../components/current-time";
 
 const Header = () => (
   <header>
     <div className="flex">
       <AmsterdamTime />
     </div>
-
-    <DarkMode />
+    <div className="flex">
+      <DarkMode />
+      <FontSwap />
+    </div>
   </header>
 );
 
