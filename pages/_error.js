bad458942a9643750c5c5a45fa58ee753bc5864c
Boris Kirov <borisasenovkirov@gmail.com>
2022-07-26 23:12:27 +0200

    fix

--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,35 @@
+import React from "react";
+
+import Motion from "../components/motion";
+import Metadata from "../components/metadata";
+import Link from "next/link";
+import MainWrapper from "../components/mainWrapper";
+import Main from "../components/innerWrapper";
+import Image from "next/image";
+
 function Error({ statusCode }) {
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : "An error occurred on client"}
-    </p>
+    <Motion>
+      <MainWrapper>
+        <Metadata
+          title="404"
+          description="Sorry, this page does not exists, but you can check my Playground."
+          image="https://www.boriskirov.me/meta-tag-404.png"
+          name="Boris Kirov"
+        />
+        <Main>
+          <div className="flex error">
+            <Image src="/sad-mac.svg" alt="404" width={124} height={124} />
+
+            <p>
+              {statusCode
+                ? `An error ${statusCode} occurred on server`
+                : "An error occurred on client"}
+            </p>
+          </div>
+        </Main>
+      </MainWrapper>
+    </Motion>
   );
 }
 
