f8275442353d5dc13d1b6c25cf8abc449f7117c4
Boris Kirov <borisasenovkirov@gmail.com>
2022-07-29 08:48:22 +0200

    strava uplift

--- components/stravaCard.js
+++ components/stravaCard.js
@@ -8,38 +8,74 @@ const StravaStats = () => {
   const RideDistance = (data?.countRides.distance / 1000).toFixed(2);
   const SwimDistance = (data?.countSwims.distance / 1000).toFixed(2);
   return (
-    <a
-      className="ApiCard"
-      href="https://www.strava.com"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <Image src="/strava.png" alt="Strava logo" width={48} height={48} />
-      <div className="flexCentered">
+    <div>
+      <div className="strava">
         <div className="flexCentered">
-          <div>
-            <p className="distance">
-              <strong>{RunDistance} km</strong>
-            </p>
-            <small className="count">{data?.countRuns.count} runs</small>
+          <div className="flexCentered">
+            <Image src="/strava.png" alt="Strava logo" width={48} height={48} />
+            <h5 className="stravaStats">My Strava stats</h5>
           </div>
-          <div>
-            <p className="distance">
-              {" "}
-              <strong> {RideDistance} km</strong>
-            </p>
-            <small className="count">{data?.countRides.count} rides</small>
+          <small className="tag stravaTag">
+            <a href="www.strava.com" target="_blank" rel="noreferrer">
+              Follow
+            </a>
+          </small>
+        </div>
+        <div className="flexCentered flexDirectionColumn">
+          <div className="flexCentered justContentBetween">
+            <Image src="/run.svg" alt="Strava logo" width={48} height={48} />
+            <div className="stravaPil">
+              <p>Distance</p>
+              <p className="distance">
+                <strong>{RunDistance} km</strong>
+              </p>
+            </div>
+            <div className="stravaPil">
+              <p>Count</p>
+              <div className="distance">
+                <strong className="count">{data?.countRuns.count} runs</strong>
+              </div>
+            </div>
           </div>
-          <div>
-            <p className="distance">
-              {" "}
-              <strong>{SwimDistance} km</strong>
-            </p>
-            <small className="count">{data?.countSwims.count} swims</small>
+          <hr />
+          <div className="flexCentered justContentBetween">
+            <Image src="/bike.svg" alt="Strava logo" width={48} height={48} />
+            <div className="stravaPil">
+              <p>Distance</p>
+              <div className="distance">
+                <p className="distance">
+                  <strong> {RideDistance} km</strong>
+                </p>
+              </div>
+            </div>
+            <div className="stravaPil">
+              <p>Count</p>
+              <div className="distance">
+                <strong className="count">{data?.countRides.count} runs</strong>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="flexCentered justContentBetween">
+            <Image src="/swim.svg" alt="Strava logo" width={48} height={48} />
+            <div className="stravaPil">
+              <p>Distance</p>
+              <div className="distance">
+                <p className="distance">
+                  <strong>{SwimDistance} km</strong>
+                </p>
+              </div>
+            </div>
+            <div className="stravaPil">
+              <p>Count</p>
+              <div className="distance">
+                <strong className="count">{data?.countSwims.count} runs</strong>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </a>
+    </div>
   );
 };
 
