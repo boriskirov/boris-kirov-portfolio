ea9695e64fb02a891f1f6fb0b44ae3c1aefcec99
Boris Kirov <borisasenovkirov@gmail.com>
2022-12-01 12:42:49 +0100

    improving the intro motion

--- components/motion.js
+++ components/motion.js
@@ -4,14 +4,14 @@ export default function Motion({ children }) {
   return (
     <motion.div
       initial={{
-        y: 5,
+        y: 7,
         opacity: 0,
       }}
       animate={{
-        y: [5, 1],
+        y: [7, 1],
         opacity: 1,
       }}
-      transition={{ ease: "easeOut", duration: 0.5 }}
+      transition={{ ease: "easeIn", duration: 0.5 }}
     >
       {children}
     </motion.div>
