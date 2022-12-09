ddb20ef138b28c055401a2593359cd7559b5cb5e
Boris Kirov <borisasenovkirov@gmail.com>
2022-12-09 11:24:52 +0100

    --

--- components/metric.js
+++ components/metric.js
@@ -1,4 +1,4 @@
 const Metric = ({ metric }) => {
-  return <h6>{metric > 0 ? metric.toLocaleString() : "-"}</h6>;
+  return <h6>{metric > 0 ? metric.toLocaleString() : "---"}</h6>;
 };
 export default Metric;
