e4b80d70aaab906ba2cfdaefc30ad53dbb438789
Boris Kirov <borisasenovkirov@gmail.com>
2023-01-11 14:57:06 +0100

    initial timeline

---
+++ components/timeline.js
@@ -0,0 +1,74 @@
+import { motion } from "framer-motion";
+import boris from "../public/boris.json";
+import Image from "next/image";
+
+const Timeline = () => {
+  return (
+    <div>
+      <ul className="timeline">
+        <li className="timeline-item">
+          <Image
+            src="/elastic.svg"
+            width={24}
+            height={24}
+            className="timeline-icon"
+          />
+          <div className="timeline-info">
+            <span>
+              <strong>{boris.experience[0].role}</strong>
+            </span>
+            <small>{boris.experience[0].company}</small>
+            <small>{boris.experience[0].dates}</small>
+          </div>
+        </li>
+        <li className="timeline-item">
+          <Image
+            src="/miro.svg"
+            width={24}
+            height={24}
+            className="timeline-icon"
+          />
+          <div className="timeline-info">
+            <span>
+              <strong>{boris.experience[1].role}</strong>
+            </span>
+            <small>{boris.experience[1].company}</small>
+            <small>{boris.experience[1].dates}</small>
+          </div>
+        </li>
+        <li className="timeline-item">
+          <Image
+            src="/info.svg"
+            width={24}
+            height={24}
+            className="timeline-icon"
+          />
+          <div className="timeline-info">
+            <span>
+              <strong>{boris.experience[2].role}</strong>
+            </span>
+            <small>{boris.experience[2].company}</small>
+            <small>{boris.experience[2].dates}</small>
+          </div>
+        </li>
+        <li className="timeline-item">
+          <Image
+            src="/despark.svg"
+            width={24}
+            height={24}
+            className="timeline-icon"
+          />
+          <div className="timeline-info">
+            <span>
+              <strong>{boris.experience[3].role}</strong>
+            </span>
+            <small>{boris.experience[3].company}</small>
+            <small>{boris.experience[3].dates}</small>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Timeline;
--- pages/about.js
+++ pages/about.js
@@ -5,12 +5,12 @@ import Link from "next/link";
 import MainWrapper from "../components/mainWrapper";
 import Main from "../components/innerWrapper";
 import Footer from "../components/Footer/footer";
-import CurrentlyListening from "../components/currentlyListening";
+
 import Image from "next/image";
 import FeedbackForm from "../components/feedbackForm";
-import StravaStats from "../components/stravaCard";
-import WebStack from "../components/Webstack/webstack";
+
 import boris from "../public/boris.json";
+import Timeline from "../components/timeline";
 
 const About = () => (
   <Motion>
@@ -38,88 +38,15 @@ const About = () => (
             <p>
               <span className="dropCap">M</span>y name is {boris.firstName} {""}
               {boris.familyName}. I'm a {boris.profession} living in{" "}
-              {boris.location.city}, {boris.location.country}. I'm developing
-              skill through doing and my care for form and function makes me
-              express myself with technology.
+              {boris.location.city}, {boris.location.country}.
             </p>
           </section>
           <section>
-            <p>
-              Currently I'm working at {boris.experience[0].company} , the
-              company behind the Elastic Stack, where we help people explore and
-              analyze data, while using the power of search.
-            </p>
-            <br />
-            <p>
-              I used to help{" "}
-              <a href="https://www.miro.com" target="_blank" rel="noreferrer">
-                {boris.experience[1].company}
-              </a>{" "}
-              on their Platform journey . Before that{" "}
-              <a href="https://info.nl/" target="_blank" rel="noreferrer">
-                {boris.experience[2].company}
-              </a>{" "}
-              &{" "}
-              <a href="https://despark.com/" target="_blank" rel="noreferrer">
-                {boris.experience[3].company}
-              </a>
-              .
-            </p>
-            <p>
-              {" "}
-              My fascination for software, programs and communciation mixed with
-              passion for constant experimentation in the realm of design and
-              experience consistency keeps me busy and always hungry. You can
-              always reach me via{" "}
-              <a
-                href="https://twitter.com/sboriskirov"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Twitter
-              </a>{" "}
-              or by booking a session on my{" "}
-              <a
-                href="https://www.adplist.org/mentors/boris-kirov"
-                target="_blank"
-                rel="noreferrer"
-              >
-                ADPList
-              </a>{" "}
-              profile.
-            </p>
+            <p>{boris.longIntro}</p>
           </section>
+
           <section>
-            <p>
-              Leaving time for sports like running, cycling, and jump rope is
-              essential for my physical and mental health. But the most
-              important one is the time for long walks with our lovely
-              monochromatic, Mozilla firefox-looking, Dory. She is an amazing,
-              smart, and incredibly responsive adopted mixed-breed dog.
-            </p>
-            <Image
-              src="/dory.png"
-              alt="Dory the Mozilla firefox-looking adopted mixed-breed dog"
-              width={672}
-              height={460}
-              layout="responsive"
-            />
-            <p>
-              Photography also is a passion of mine, with which I keep my
-              creative juices flowing. I am fascinated by film photography and
-              currently, I'm also deep into drone photography. You can check
-              some of my flickaronies on{" "}
-              <a
-                href="https://instagram.com/yng.blds"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Instagram
-              </a>
-              . Before I used to collaborate with clients like Tea House Sofia,
-              FITC Amsterdam, and others.
-            </p>
-            <WebStack />
+            <Timeline />
           </section>
 
           <FeedbackForm
