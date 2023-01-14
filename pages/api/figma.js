95ade3ec6e7b906c051a8c254eb48002b1f6fae0
Boris Kirov <borisasenovkirov@gmail.com>
2023-01-14 13:53:29 +0100

    adding more stats in figma

--- pages/api/figma.js
+++ pages/api/figma.js
@@ -1,13 +1,25 @@
 async function Figma(req, res) {
-  const figma = await fetch(
+  const notes = await fetch(
     "https://www.figma.com/api/plugins/943057200269205449/versions"
   );
 
-  const json = await figma.json();
-  const figmaRuns = json.meta.plugin.unique_run_count;
-  const figmaViews = json.meta.plugin.view_count;
-  const figmaInstalls = json.meta.plugin.install_count;
-  const figmaLikes = json.meta.plugin.like_count;
+  const mirotone = await fetch(
+    "https://www.figma.com/api/plugins/1040629471915005702/versions"
+  );
+
+  const jsonNotes = await notes.json();
+  const jsonMirotone = await mirotone.json();
+
+  const figmaRuns =
+    jsonNotes.meta.plugin.unique_run_count +
+    jsonMirotone.meta.plugin.unique_run_count;
+  const figmaViews =
+    jsonNotes.meta.plugin.view_count + jsonMirotone.meta.plugin.view_count;
+  const figmaInstalls =
+    jsonNotes.meta.plugin.install_count +
+    jsonMirotone.meta.plugin.install_count;
+  const figmaLikes =
+    jsonNotes.meta.plugin.like_count + jsonMirotone.meta.plugin.like_count;
 
   return res.status(200).json({
     figmaRuns,
