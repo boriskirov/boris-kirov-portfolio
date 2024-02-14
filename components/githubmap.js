d6f2ab83b7562602f9630b9b2c7d65b8d4f2a9f8
Boris Kirov <borisasenovkirov@gmail.com>
2024-02-14 15:53:23 +0100

    fixed

--- components/githubmap.js
+++ components/githubmap.js
@@ -3,7 +3,7 @@ export default function GithubMap({}) {
     <div className="tag">
       <img
         src="https://ghchart.rshah.org/f06a8b/boriskirov"
-        alt="Boris Github chart"
+        alt="Boris Github contribution chart"
       ></img>
     </div>
   );
